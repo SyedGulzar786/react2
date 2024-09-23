b267a7ae7f8aa6acec88f160233d54db47884e44
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2024-09-23 05:47:34 +0500

    react bootsrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import {Card, Input} from "./components"
+import Button from 'react-bootstrap/Button';
 function App() {
   const getData = (a) => {
     console.log(a)
   }
   const arr = ["one","two","three"]
   return (
-    <div className="App">
+<div className="App">
+<Button variant="dark">Dark</Button>
 <Input onChange={(e) => console.log(e.target.value)} />
 {arr.map((v,i) => <Card v={v} getData={getData} />)}
-    </div>
+</div>
   );
 }
 
