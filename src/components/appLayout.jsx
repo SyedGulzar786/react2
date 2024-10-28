4585118e8dedfb7b609b4789afb1ff82cf43a609
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2024-10-28 05:31:51 +0500

    shifting pages through react-router-dom byy two ways link component and useNavigate( ) Hook

---
+++ src/components/appLayout.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { useNavigate, Link } from 'react-router-dom';
+const { Header, Content, Footer } = Layout;
+const items = [
+    {
+        label: "Home",
+        key: "/home",
+    },
+    {
+        label: "About",
+        key: "/about",
+    },
+    {
+        label: "Profile",
+        key: "/profile",
+    },
+    {
+        label: "Login",
+        key: "/",
+    },
+]
+const AppLayout = ({ children }) => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  const navigate = useNavigate();
+  return (
+    <Layout>
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="demo-logo" />
+        {/* <Menu
+          onClick={(data) => {navigate(data.key)}}
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        /> */}
+        <div style={{display: 'flex',width: '100%',gap: '10px',}}>
+
+        <Link to="/home" >
+        <div style={{ cursor: 'pointer',color: 'white' }}>Home</div>
+        </Link>
+        <Link to="/profile">
+        <div style={{ cursor: 'pointer',color: 'white' }}>Profile</div>
+        </Link>
+        <Link to="/about" >
+        <div style={{ cursor: 'pointer',color: 'white' }}>About</div>
+        </Link>
+        <Link to="/">
+        <div style={{ cursor: 'pointer',color: 'white' }}>Login</div>
+        </Link>
+
+        </div>
+      </Header>
+      <Content
+        style={{
+          margin: '24px 16px',
+          padding: '40px 48px'
+        }}
+      >
+        {/* <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb> */}
+        <div
+          style={{
+            padding: 24,
+            height: '650px',
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          {children}
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+export default AppLayout;
--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,15 @@ import About from "../pages/about";
 import Profile from "../pages/profile";
 import Login from "../pages/login";
 import Layouti from "../components/ant_layout";
+import AppLayout from "../components/appLayout";
 export default function AppRouter(){
     return(
         <BrowserRouter>
         <Routes>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/profile" element={<Layouti><Profile/></Layouti>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/" element={<Login/>}/>
+            <Route path="/home" element={<AppLayout><Home/></AppLayout>}/>
+            <Route path="/profile" element={<AppLayout><Profile/></AppLayout>}/>
+            <Route path="/about" element={<AppLayout><About/></AppLayout>}/>
+            <Route path="/" element={<AppLayout><Login/></AppLayout>}/>
         </Routes>
         </BrowserRouter>
     )
